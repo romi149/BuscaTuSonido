3040cae0fb045f1b8c2f64a8febb8b8b2dbf88dd
Romina Castellanos <RominaSoledad.Castellanos@Alumnos.uai.edu.ar>
2020-08-24 22:03:07 -0300

    Se crea la estructura general del proyecto y se generan los mapper para usuario

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,39 @@
+﻿using MPP;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var resultado = new MapperUsuario().ListarUsuarios();
+            //resultado.ForEach(x => Console.WriteLine($"Nombre de Usuario; {x.Nombre}"));
+
+            //Console.WriteLine(new string('*',50));
+            //Console.WriteLine(new string('*',50));
+            //Console.WriteLine(new string('*',50));
+            //bool realizado=new MapperUsuario().InsertarUsuario(new DAL.Usuario { Nombre = "Adrian" });
+
+            //var resultados = new MapperUsuario().ListarUsuarios();
+            //resultados.ForEach(x => Console.WriteLine($"Nombre de Usuario; {x.Nombre}"));
+            //Console.WriteLine(new string('*', 50));
+            //Console.WriteLine(new string('*', 50));
+            //Console.WriteLine(new string('*', 50));
+            //bool realizado2 = new MapperUsuario().Actualizar(new DAL.Usuario {IdUsuario=4, Nombre = "Adrian" });
+            //resultados = new MapperUsuario().ListarUsuarios();
+            //resultados.ForEach(x => Console.WriteLine($"Nombre de Usuario; {x.Nombre}"));
+            Console.WriteLine(new string('*', 50));
+            Console.WriteLine(new string('*', 50));
+            Console.WriteLine(new string('*', 50));
+            bool realizado3 = new MapperUsuario().Borrar(new DAL.Usuario { IdUsuario = 4 });
+            var resultados = new MapperUsuario().ListarUsuarios();
+            resultados.ForEach(x => Console.WriteLine($"Nombre de Usuario; {x.Nombre}"));
+            Console.ReadLine();
+        }
+    }
+}
---
+++ MPP/MapperUsuario.cs
@@ -0,0 +1,97 @@
+﻿using DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace MPP
+{
+    public class MapperUsuario
+    {
+        /// <summary>
+        /// Retorna todos los usuarios de la Bd
+        /// </summary>
+        /// <returns></returns>
+        public List<Usuario> ListarUsuarios()
+        {
+            try
+            {
+                BTSDataContext BaseDeDatos = new BTSDataContext();
+                List<Usuario> usuarios = (from tblUsuarios in BaseDeDatos.Usuario
+                                          select tblUsuarios).ToList();
+                return usuarios;
+            }
+            catch (Exception e)
+            {
+
+                return new List<Usuario>();
+            }
+        }
+
+        /// <summary>
+        /// Inserta un usuario en Bd
+        /// </summary>
+        /// <param name="usuario">Tipo Usuario</param>
+        /// <returns>Devuelve si se inserto O no</returns>
+        public bool InsertarUsuario(Usuario usuario)
+        {
+            BTSDataContext BaseDeDatos = new BTSDataContext();
+            int filasAFECTADAS= BaseDeDatos.InsertarUsuario(usuario.Usuario1, usuario.Nombre,
+                usuario.Apellido, 
+                usuario.Pass, usuario.Estado,
+                usuario.IdIdioma, usuario.Dni);
+            if (filasAFECTADAS > 0)
+            {
+                return true;
+            }            
+            
+            return false;
+        }
+
+
+        /// <summary>
+        /// Inserta un usuario en Bd
+        /// </summary>
+        /// <param name="usuario">Tipo Usuario</param>
+        /// <returns>Devuelve si se inserto O no</returns>
+        public bool Actualizar(Usuario usuario)
+        {
+            BTSDataContext BaseDeDatos = new BTSDataContext();
+            int filasAFECTADAS = BaseDeDatos.ActualizarUsuario(usuario.IdUsuario,usuario.Usuario1,
+                usuario.Nombre,
+                usuario.Apellido,
+                usuario.Pass, usuario.Estado,
+                usuario.IdIdioma, usuario.Dni);
+            if (filasAFECTADAS > 0)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// Inserta un usuario en Bd
+        /// </summary>
+        /// <param name="usuario">Tipo Usuario</param>
+        /// <returns>Devuelve si se inserto O no</returns>
+        public bool Borrar(Usuario usuario)
+        {
+            BTSDataContext BaseDeDatos = new BTSDataContext();
+            int filasAFECTADAS = BaseDeDatos.Borrarusuario(usuario.IdUsuario, 1);
+        
+            if (filasAFECTADAS > 0)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+
+
+
+    }
+}
